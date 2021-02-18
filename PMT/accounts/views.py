922dd3a37567462ebb5bc303b8127455b9515b67
carsondotpy <42276605+carsonalexander14@users.noreply.github.com>
2021-02-18 09:34:39 -0600

    projects_list

--- PMT/accounts/views.py
+++ PMT/accounts/views.py
@@ -4,35 +4,12 @@ from django.contrib.auth import login, logout
 from accounts.forms import EditProfileForm
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm
 from accounts.models import User 
+from projects.models import Project, Position, Application
 
 from django.urls import reverse_lazy
 from django.views import generic
 
 from . import forms
-
-#login, logout, signup
-
-# class LoginView(generic.FormView):
-    #form_class = AuthenticationForm
-    #success_url = reverse_lazy("PMT:home")
-    #template_name = "accounts/signin.html"
-    
-    #def get_form(self, form_class=None):
-        #if form_class is None:
-            #form_class = self.get_form_class()
-        #return form_class(self.request, **self.get_form_kwargs())
-    
-    #def form_valid(self, form):
-        #login(self.request, form.get_user())
-        #return super().form_valid(form)
-
-
-#class LogoutView(generic.RedirectView):
-    #url = reverse_lazy("home")
-    
-    #def get(self, request, *args, **kwargs):
-        #logout(request)
-        #return super().get(request, *args, **kwargs)
 
 
 class SignUp(generic.CreateView):
@@ -40,11 +17,19 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy("login")
     template_name = "accounts/signup.html"
 
+
 #profile 
 @login_required
 def profile(request):
-    context = {'user': request.user}
+
+    projects_list = Project.objects.filter(owner=request.user)
+
+    context = {
+        'user': request.user,
+        'projects_list': projects_list,
+    }
     return render(request, 'accounts/profile.html', context)
+
 
 @login_required
 def profile_edit(request):
