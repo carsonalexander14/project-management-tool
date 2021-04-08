9bce2835a19e6087c42ce1fa33209e6e4f67e261
carsondotpy <carson.alexander14@gmail.com>
2021-04-08 14:36:56 -0500

    delete_env

--- PMT/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\carso_q2hl7ak\onedrive\desktop\project-management-tool\pmt\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
