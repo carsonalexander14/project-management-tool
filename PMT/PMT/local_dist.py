3d737fe9412329f1bd2ce2014f40b6e54b24a6c8
carsondotpy <carson.alexander14@gmail.com>
2021-04-15 10:27:31 -0500

    local update

---
+++ PMT/PMT/local_dist.py
@@ -0,0 +1,20 @@
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'TotallyRealSecretKey'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
